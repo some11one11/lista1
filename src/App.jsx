d94c9f4930aeb227b0f3f6e08fade4692c9deea7
GUSTAVO KAULE DOS SANTOS <gustavo_santos178@estudante.sesisenai.org.br>
2026-03-17 15:36:22 -0300

    primeiro commit :D

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+return (
+  <p> FAÇAM O APP AI TALIGADO</p>
+)
+}
+export default App
